f81951383c59ddffd2c954927693193724649ee3
Bianca Alves <biancamalves1905@gmail.com>
2026-07-22 16:27:45 -0300

    feat(home): implementa seção indicators

--- Frontend/src/pages/Home/Home.tsx
+++ Frontend/src/pages/Home/Home.tsx
@@ -1,9 +1,16 @@
 import Hero from "../../components/sections/Home/Hero/Hero";
+import Indicators from "../../components/sections/Home/Indicators/Indicators";
 
 export default function Home() {
   return (
-    <>
+   <main className="home">
       <Hero />
-    </>
+      <Indicators />
+      {/* Services */}
+      {/* Partners */}
+      {/* Testimonials */}
+      {/* CTA */}
+    </main>
   );
 }
+  
